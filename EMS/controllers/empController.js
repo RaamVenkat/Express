cffe640c56d531468a93e30050d78594d8b431f1
RaamVenkat <v.raamaanujam@gmail.com>
2023-05-29 09:48:15 +0530

    viewone

--- EMS/controllers/empController.js
+++ EMS/controllers/empController.js
@@ -1,10 +1,38 @@
 import Employee from '../models/empModel.js';
 
-export const addEmp = async (req, res) => {};
+export const addEmp = async (req, res) => {
+	try {
+		const { firstName, middleName = '', lastName, email, role, dob, empCode } = req.body;
+		const newEmp = new Employee({
+			firstName,
+			middleName,
+			lastName,
+			email,
+			role,
+			dob,
+			empCode,
+		});
+
+		await newEmp.save();
+		res.status(201).json(`${empCode} Employee added`);
+	} catch (err) {
+		res.status(409).json({ message: err.message });
+	}
+};
 
 export const viewEmp = async (req, res) => {
 	try {
 		const emp = await Employee.find();
+		res.status(200).json(emp);
+	} catch (err) {
+		res.status(404).json({ message: err.message });
+	}
+};
+
+export const viewOneEmp = async (req, res) => {
+	try {
+		const empCode = req.params;
+		const emp = await Employee.findOne(empCode);
 		res.status(200).json(emp);
 	} catch (err) {
 		res.status(404).json({ message: err.message });
--- EMS/routes/empRoutes.js
+++ EMS/routes/empRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { addEmp, viewEmp } from '../controllers/empController.js';
+import { addEmp, viewEmp, viewOneEmp } from '../controllers/empController.js';
 import verifyToken from '../middleware/validateToken.js';
 const router = express.Router();
 
 router.post('/add', addEmp);
 router.get('/view', viewEmp);
+router.get('/view/:empCode', viewOneEmp);
 
 export default router;
